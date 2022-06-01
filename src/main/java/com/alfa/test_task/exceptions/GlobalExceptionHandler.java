3f8c6ed9876f62e7a2282837b1b73f2097866d49
danilaosipov <jgj>
2022-06-01 19:47:46 +0400

    Added logging for runtime exceptions

--- src/main/java/com/alfa/test_task/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/alfa/test_task/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.alfa.test_task.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -15,6 +17,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<String> handleRuntimeException(RuntimeException exception) {
+        log.warn("Runtime exception happened, message: " + exception.getMessage());
         return new ResponseEntity<>("Something went wrong, try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
